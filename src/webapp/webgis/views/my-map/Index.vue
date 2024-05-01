5fcdf2b6a4dc9f88cb82eb0bbcf01aebe89462d1
huangjunru <huang.junru@datasw.com>
2024-05-01 12:25:18 +0800

    地图工具

--- src/webapp/webgis/views/my-map/Index.vue
+++ src/webapp/webgis/views/my-map/Index.vue
@@ -1,11 +1,17 @@
 <template>
 	<div class="map-view">
+		<!-- 地图 -->
 		<cesium-map />
+		<!-- 地图工具 -->
+		<div class="map-bottom-tools-wrapper">
+			<map-bottom-tools />
+		</div>
 	</div>
 </template>
 
 <script lang="ts" setup>
 import CesiumMap from '@/components/cesium-map'
+import MapBottomTools from './components/map-bottom-tools/Index.vue'
 </script>
 
 <style lang="less" scoped>
@@ -13,5 +19,11 @@ import CesiumMap from '@/components/cesium-map'
 	position: relative;
   width: 100%;
   height: 100%;
+	.map-bottom-tools-wrapper {
+		position: absolute;
+		bottom: 20px;
+		right: 20px;
+		z-index: 2;
+	}
 }
 </style>
---
+++ src/webapp/webgis/views/my-map/components/map-view-info/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
